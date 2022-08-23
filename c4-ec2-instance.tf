206a46d28c518fca881a44c961839f32c62ea993
aleemayaana <107088988+aleemayaana@users.noreply.github.com>
2022-08-23 12:13:06 -0400

    fdf

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -1,6 +1,7 @@
 # Create EC2 Instance - Amazon2 Linux
 resource "aws_instance" "my-ec2-vm" {
-  ami           = data.aws_ami.amzlinux.id 
+  ami           = data.aws_ami.amzlinux.id
+  count = 1
   instance_type = var.instance_type
    vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
