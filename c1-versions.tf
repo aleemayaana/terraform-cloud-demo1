2ef656874887f7cbba342fbc021d1a317dde284f
aleemayaana <107088988+aleemayaana@users.noreply.github.com>
2022-08-23 16:06:57 -0400

    rwerwer

--- c1-versions.tf
+++ c1-versions.tf
@@ -10,9 +10,9 @@ terraform {
 }
 
 # Provider Block
-#provider "aws" {
-  #region  = var.aws_region
-#}
+provider "aws" {
+ region  = var.aws_region
+}
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
