cdf29ce93196a7529e2ebf574a52d0e9639cdec5
aleemayaana <107088988+aleemayaana@users.noreply.github.com>
2022-08-24 13:36:55 -0400

    dsfsdf

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id
   count = 1
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
    vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
     "Name" = "Terraform-Cloud-${count.index}"
